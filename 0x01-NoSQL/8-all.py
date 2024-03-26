23fd6a9fbd96f017af02d14b44f2dcac8f8e5d4e
MosesChisz <mosesc@cortexware.co.za>
2024-03-26 09:21:21 +0000

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+'''Task 8's module.
+'''
+
 
 def list_all(mongo_collection):
-    documents = []
-    try:
-        cursor = mongo_collection.find({})
-        for document in cursor:
-            documents.append(document)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    return documents
+    '''Lists all documents in a collection.
+    '''
+    return [doc for doc in mongo_collection.find()]
